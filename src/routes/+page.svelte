62a7e227171186cf0887563477ef909f94aed4cd
Mahinkumar <73975593+Mahinkumar@users.noreply.github.com>
2024-11-01 16:07:42 +0530

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+
+<div class="flex w-screen h-screen flex-col items-center justify-center bg-neutral-950 text-white relative">
+	<div class="text-3xl">Mahinkumar - Portfolio</div>
+    
+    <div class="flex justify-evenly items-center lg:flex-row flex-col w-[30%] pt-10 text-cyan-500 underline text-xl">
+        <a href="https://github.com/mahinkumar">Github</a>
+        <a href="https://docs.mahinkumar.com/">Docs</a>
+        <a href="https://blog.mahinkumar.com/">Blog</a>
+        <a href="https://linkedin.com/in/mahinkumar/">Linkedin</a>
+    </div>
+    <div class="h-10 text-lg absolute top-1 bg-yellow-400/10 w-full flex  justify-center items-center border-yellow-800/50 border-b-2">The site is currently under construction</div>
+</div>
